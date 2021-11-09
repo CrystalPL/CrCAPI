49ec33f2115ab0e082e06f9a0dbf228f8198e564
Crystal <janwolowski@crystalek.pl>
2021-11-09 18:23:50 +0100

    create plugin.yml files

--- gui/src/main/java/pl/crystalek/crcapi/gui/CrCAPIGUI.java
+++ gui/src/main/java/pl/crystalek/crcapi/gui/CrCAPIGUI.java
@@ -2,5 +2,5 @@ package pl.crystalek.crcapi.gui;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CrCGUIApi extends JavaPlugin {
+public final class CrCAPIGUI extends JavaPlugin {
 }
