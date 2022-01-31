a166ccf0153a3f80ce013af1c9ab8799aa299260
Crystal <janwolowski@crystalek.pl>
2022-01-31 23:06:09 +0100

    loadCommands throw one knowing exception

--- command/src/main/java/pl/crystalek/crcapi/command/loader/CommandLoader.java
+++ command/src/main/java/pl/crystalek/crcapi/command/loader/CommandLoader.java
@@ -7,15 +7,21 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.crystalek.crcapi.command.impl.SingleCommand;
 import pl.crystalek.crcapi.command.model.CommandData;
+import pl.crystalek.crcapi.core.config.exception.ConfigLoadException;
 
 import java.io.IOException;
 import java.util.*;
 
 @UtilityClass
 public class CommandLoader {
-    public Optional<Map<Class<? extends SingleCommand>, CommandData>> loadCommands(final ConfigurationSection commandsConfigurationSection, final ClassLoader classLoader, final JavaPlugin plugin) throws ClassNotFoundException, IOException {
+    public Optional<Map<Class<? extends SingleCommand>, CommandData>> loadCommands(final ConfigurationSection commandsConfigurationSection, final ClassLoader classLoader, final JavaPlugin plugin) throws ConfigLoadException {
         final Map<Class<? extends SingleCommand>, CommandData> commandDataMap = new HashMap<>();
-        final Set<ClassInfo> classList = ClassPath.from(classLoader).getAllClasses();
+        final Set<ClassInfo> classList;
+        try {
+            classList = ClassPath.from(classLoader).getAllClasses();
+        } catch (final IOException exception) {
+            throw new ConfigLoadException("Error while getting all classes from classloader");
+        }
 
         for (final String defaultCommandName : commandsConfigurationSection.getKeys(false)) {
             final ConfigurationSection commandConfigurationSection = commandsConfigurationSection.getConfigurationSection(defaultCommandName);
@@ -52,10 +58,17 @@ public class CommandLoader {
         return Optional.of(commandDataMap);
     }
 
-    private Optional<Class<? extends SingleCommand>> getCommand(final String commandName, final Set<ClassInfo> classList, final ClassLoader classLoader, final JavaPlugin plugin) throws ClassNotFoundException {
+    private Optional<Class<? extends SingleCommand>> getCommand(final String commandName, final Set<ClassInfo> classList, final ClassLoader classLoader, final JavaPlugin plugin) throws ConfigLoadException {
         for (final ClassInfo classInfo : classList) {
             if (classInfo.getSimpleName().equalsIgnoreCase(commandName)) {
-                final Class<?> clazz = Class.forName(classInfo.getName(), false, classLoader);
+                final Class<?> clazz;
+                try {
+
+                    clazz = Class.forName(classInfo.getName(), false, classLoader);
+                } catch (final ClassNotFoundException exception) {
+                    throw new ConfigLoadException("Not found class: " + commandName);
+                }
+
                 if (clazz.getSuperclass().equals(SingleCommand.class)) {
                     return Optional.of((Class<? extends SingleCommand>) clazz);
                 }
